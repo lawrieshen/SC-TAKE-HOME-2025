85c1308a3b29463602e0b7e3e27e1d2434fe4bcb
Lawrence <lawrenceshen1213@gmail.com>
2024-09-30 23:16:45 -0400

    feat: implement GetAllChildFolders func

--- folder/get_folder.go
+++ folder/get_folder.go
@@ -1,6 +1,11 @@
 package folder
 
-import "github.com/gofrs/uuid"
+import (
+	"fmt"
+
+	"errors"
+	"github.com/gofrs/uuid"
+)
 
 func GetAllFolders() []Folder {
 	return GetSampleData()
@@ -20,8 +25,41 @@ func (f *driver) GetFoldersByOrgID(orgID uuid.UUID) []Folder {
 
 }
 
-func (f *driver) GetAllChildFolders(orgID uuid.UUID, name string) []Folder {
-	// Your code here...
+func (f *driver) GetAllChildFolders(orgID uuid.UUID, name string) ([]Folder, error) {
+	folders := f.folders
+	var parentFolder *Folder
 
-	return []Folder{}
+
+	// Find parent folder
+	for _, folder := range folders {
+		if folder.OrgId == orgID && folder.Name == name {
+			parentFolder = &folder
+			break
+		}
+	}
+
+	// Handle parent folder not found error
+	if parentFolder == nil {
+
+		for _, folder := range folders {
+			if folder.Name == name {
+				fmt.Printf("Found folder with name: %s but OrgID does not match\n", folder.Name) // Debug: Folder name exists, but OrgID mismatch
+				return nil, errors.New("Folder does not exist in the specified organization")
+			}
+		}
+		return nil, errors.New("Folder does not exist")
+	}
+
+
+	// Find the child folders by validating the path prefix
+	res := []Folder{}
+	for _, folder := range folders {
+		if folder.OrgId == orgID && len(folder.Paths) >= len(parentFolder.Paths) &&
+			folder.Paths[:len(parentFolder.Paths)] == parentFolder.Paths {
+			res = append(res, folder)
+		}
+	}
+
+	return res, nil
 }
+
